b277b6c08c8d94116e9656cbf9f159e017df1896
Conor <conorotompkins@gmail.com>
2020-04-02 18:27:12 -0400

    added code that munges and summarized allegheny census tract stats from LODES

--- scripts/munge_data.R
+++ scripts/munge_data.R
@@ -1,13 +1,78 @@
 library(tidyverse)
 library(sf)
 library(janitor)
+library(tidycensus)
+library(sf)
+library(tigris)
+library(vroom)
+
+#query census
+allegheny_tracts <- get_decennial(geography = "tract",
+                                  variables = c(total_pop = "P001001"),
+                                  state = "PA",
+                                  county = "Allegheny County",
+                                  geometry = TRUE,
+                                  output = "wide")
+
+#read crosswalk
+geo_crosswalk <- vroom("data/pa_xwalk.csv.gz", col_types = cols(.default = "c"))
+
+geo_crosswalk
+
+#read in lodes data
+df_lodes <- vroom("data/pa_od_main_JT00_2017.csv.gz", col_types = cols(.default = "c")) %>% 
+  mutate(S000 = as.numeric(S000)) %>% 
+  select(h_geocode, w_geocode, S000)
+
+df_lodes
+
+#summarize lodes data
+df_tracts_summarized <- df_lodes %>% 
+  group_by(h_geocode, w_geocode) %>% 
+  summarize(commuters = sum(S000)) %>% 
+  ungroup() %>% 
+  arrange(desc(commuters))
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  left_join(geo_crosswalk %>% select(tabblk2010, trct), by = c("h_geocode" = "tabblk2010")) %>% 
+  rename(h_tract = trct) %>% 
+  left_join(geo_crosswalk %>% select(tabblk2010, trct), by = c("w_geocode" = "tabblk2010")) %>% 
+  rename(w_tract = trct)
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  group_by(h_tract, w_tract) %>% 
+  summarize(commuters = sum(commuters)) %>% 
+  ungroup() %>% 
+  arrange(desc(commuters))
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  semi_join(allegheny_tracts, by = c("h_tract" = "GEOID")) %>% 
+  semi_join(allegheny_tracts, by = c("w_tract" = "GEOID"))
+
+df_home <- df_tracts_summarized %>% 
+  rename(tract = h_tract,
+         residents = commuters) %>% 
+  select(-w_tract) %>% 
+  group_by(tract) %>% 
+  summarize(residents = sum(residents))
+
+df_work <- df_tracts_summarized %>% 
+  rename(tract = w_tract,
+         jobs = commuters) %>% 
+  select(-h_tract) %>% 
+  group_by(tract) %>% 
+  summarize(jobs = sum(jobs))
+
+df_summarized_census_tract_residents_jobs <- allegheny_tracts %>% 
+  left_join(df_home, by = c("GEOID" = "tract")) %>% 
+  left_join(df_work, by = c("GEOID" = "tract"))
+
+#st_write(df_summarized_census_tract_residents_jobs, "data/shapefiles/summarized_census_tract_residents_jobs.shp")
 
 
 #summarized tract data
 df_tract_centroid_summary <- st_read("data/shapefiles/summarized_census_tract_residents_jobs/summarized_census_tract_residents_jobs.shp") %>%
   st_transform(4326) %>% 
-  rename(jobs = cmmtrs_n,
-         residents = cmmtrs_t) %>% 
   mutate(lon = map_dbl(geometry, ~st_centroid(.x)[[1]]),
          lat = map_dbl(geometry, ~st_centroid(.x)[[2]])) %>% 
   st_drop_geometry() %>% 
@@ -15,8 +80,6 @@ df_tract_centroid_summary <- st_read("data/shapefiles/summarized_census_tract_re
   st_transform(4326)
 
 #st_write(df_tract_centroid_summary, "data/shapefiles/tract_centroid_summary/tract_centroid_summary.shp")
-
-
 
 ##load transit data
 transit_lines <- st_read("data/shapefiles/transit_lines/PAAC_Routes_1909.shp") %>%
@@ -32,7 +95,7 @@ df_service_type <- transit_lines %>%
   distinct(service_type, route_id) %>% 
   st_drop_geometry()
 
-str(transit_lines)
+glimpse(df_service_type)
 
 transit_stops <- st_read("data/shapefiles/transit_stops/PAAC_Stops_1909.shp") %>%
   st_transform(4326) %>% 
@@ -41,7 +104,7 @@ transit_stops <- st_read("data/shapefiles/transit_stops/PAAC_Stops_1909.shp") %>
   select(stop_name, routes_served = routes_ser, routes_cou, geometry) %>% 
   distinct(stop_name, routes_served = routes_served, routes_cou, geometry)
 
-str(transit_stops)
+glimpse(transit_stops)
 
 max_routes_served <- transit_stops %>% 
   summarize(max_routes = max(routes_cou)) %>% 
@@ -62,13 +125,11 @@ transit_stops <- transit_stops %>%
   pivot_longer(cols = starts_with("route_"), names_to = "route_number", values_to = "route_id") %>% 
   st_as_sf()
 
-str(transit_stops)
+glimpse(transit_stops)
 
 transit_stops <- transit_stops %>%
   filter(!is.na(route_id)) %>% 
   left_join(df_service_type)
-
-
 
 #join stops to tract centroids
 df_joined <- transit_stops %>% 
